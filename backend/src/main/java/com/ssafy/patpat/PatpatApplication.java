269486d83d03f3d60a52f45aaf2b766a834d74b0
dlwogur0415 <dlwogur0415@icloud.com>
2023-01-17 17:31:28 +0900

    extra : 백엔드 초기 세팅

---
+++ backend/src/main/java/com/ssafy/patpat/PatpatApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.patpat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatpatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatpatApplication.class, args);
+	}
+
+}
