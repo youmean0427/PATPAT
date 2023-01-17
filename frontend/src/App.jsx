3ea328d47790792e5a52f5d8d090c48f7606f2d1
kamoo2 <kamoon2da@gmail.com>
2023-01-17 23:02:40 +0900

    chore : styled-reset 설치해 reset css 적용

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import { Outlet } from 'react-router-dom';
 import Footer from './components/common/Footer/Footer';
 import Header from './components/common/Header/Header';
-
+import { Reset } from 'styled-reset';
 const App = () => {
   return (
     <>
+      <Reset />
       <Header />
       <Outlet />
       <Footer />
