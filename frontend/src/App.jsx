7ed681fd5f6dbd61f08512cb0b49e2a07f6cca7b
tjrghks96 <tjrghks96@naver.com>
2023-01-17 11:11:17 +0900

    SETTING : 추가 설정 마무리

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from 'react-router-dom';
-import Footer from './components/Footer/Footer';
-import Header from './components/Header/Header';
+import Footer from './components/common/Footer/Footer';
+import Header from './components/common/Header/Header';
 
 const App = () => {
   return (
--- frontend/src/context/DarkModeContext.jsx
+++
@@ -1,1 +0,0 @@
-// Context API 관련
