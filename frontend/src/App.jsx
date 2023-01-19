3eb6721eef33d98e77c5cf8413155ca66e50c648
tjrghks96 <tjrghks96@naver.com>
2023-01-19 13:40:43 +0900

    S08P12E104-78 #comment feature/front/layout:HomeLayout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import Footer from 'components/Common/Footer/Footer';
 import { Outlet } from 'react-router-dom';
 import Header from './components/Common/Header/Header';
-import styles from 'assets/styles/App.module.scss';
+import styles from 'App.module.scss';
+import SideMenu from 'components/Common/SideMenu/SideMenu';
 
 const App = () => {
   return (
     <div className={styles.app}>
       <Header />
+      <SideMenu />
       <main className={styles['app-main']}>
         <Outlet />
       </main>
