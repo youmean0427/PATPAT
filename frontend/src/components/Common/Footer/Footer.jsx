f8af5cc223420366293f1131294a66051d3bb7c5
tjrghks96 <tjrghks96@naver.com>
2023-01-20 15:56:40 +0900

    #S08P12E104-80 #comment feat:feature/front/layout:[Footer 레이아웃에 Container 컴포넌트 추가]

--- frontend/src/components/Common/Footer/Footer.jsx
+++ frontend/src/components/Common/Footer/Footer.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import Container from '../Container';
 import styles from './Footer.module.scss';
 export default function Footer() {
-  return <footer className={styles.footer}>Footer</footer>;
+  return (
+    <footer className={styles.footer}>
+      <Container>Footer</Container>
+    </footer>
+  );
 }
