a58bf9c624fc868570d27d14dbc05eb82098d2aa
rudgns <rudgns8285@naver.com>
2023-01-24 20:16:49 +0900

    #S08P12E104-43 #todo #comment 'entity와 repogitory 작업중'

--- backend/src/main/java/com/ssafy/patpat/user/entity/User.java
+++ backend/src/main/java/com/ssafy/patpat/user/entity/User.java
@@ -1,4 +1,45 @@
 package com.ssafy.patpat.user.entity;
 
+import com.sun.istack.NotNull;
+import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicInsert;
+
+import javax.persistence.*;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@DynamicInsert
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(name = "age_range")
+    @NotNull
+    private String ageRange;
+
+    @Column(name = "nickname")
+    @NotNull
+    private String nickname;
+
+    @Column(name = "exp")
+    @ColumnDefault("0")
+    private Integer exp;
+
+    @Column(name = "email")
+    @NotNull
+    private String email;
+
+    @Column(name = "profile_image")
+    private String profileImage;
+
+    @Column(name = "refresh_token")
+    @NotNull
+    private String refreshToken;
+
 }
