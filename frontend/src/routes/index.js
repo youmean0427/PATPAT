e6c761e1b6e8f29f4a4bc2d318eac1c2f9180420
youmean0427 <youmean0427@naver.com>
2023-01-25 09:36:17 +0900

    chore:index.js 수정

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -1,26 +1,39 @@
 import { createBrowserRouter } from 'react-router-dom';
-import App from '../App';
-import Home from '../pages/Home/Home';
-import Login from '../pages/Login/Login';
-import Intro from '../pages/Intro/Intro';
-import Vision from '../pages/Intro/Vision';
-import Story from '../pages/Intro/Story';
-import Statistics from '../pages/Intro/Statistics';
-import NotFound from '../pages/NotFound/NotFound';
+import MainLayout from 'layouts/MainLayout';
+import Home from 'pages/Home/Home';
+import Login from 'pages/Login/Login';
+import NotFound from 'pages/NotFound/NotFound';
+import MbtiLayout from 'layouts/MbtiLayout';
+import Result from 'pages/Mbti/Result';
+import Start from 'pages/Mbti/Start';
+import Test from 'pages/Mbti/Test';
+import Intro from 'pages/Intro/Intro';
+import Statistics from 'pages/Intro/Statistics';
+import Story from 'pages/Intro/Story';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <App />,
+    element: <MainLayout />,
     errorElement: <NotFound />, // 라우터에 없는 경로로 이동시 NotFound 컴포넌트 화면에 띄운다.
     children: [
       { index: true, path: '/', element: <Home /> },
       { path: 'home', element: <Home /> },
       { path: 'login', element: <Login /> },
       { path: 'intro', element: <Intro /> },
+      { path: 'statistics', element: <Statistics /> },
       { path: 'vision', element: <Vision /> },
       { path: 'story', element: <Story /> },
-      { path: 'statistics', element: <Statistics /> },
+    ],
+  },
+  {
+    path: '/mbti',
+    element: <MbtiLayout />,
+    errorElement: <NotFound />,
+    children: [
+      { index: true, element: <Start /> },
+      { path: 'test', element: <Test /> },
+      { path: 'result', element: <Result /> },
     ],
   },
 ]);
