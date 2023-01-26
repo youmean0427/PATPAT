18821dbbe775c2540d62ee38ada911a739654159
tjrghks96 <tjrghks96@naver.com>
2023-01-26 17:50:36 +0900

    chore : react-query 추가 , home 페이지 mock json 파일 , index.js의 React.StrickMode 제거 , gray-color 추가

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,11 +5,14 @@ import router from './routes';
 import 'assets/styles/index.scss';
 import { CircularProgress } from '@mui/material';
 import registerServiceWorker from './registerServiceWorker';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <QueryClientProvider client={queryClient}>
     <RouterProvider router={router} fallbackElement={<CircularProgress />} />
-  </React.StrictMode>
+  </QueryClientProvider>
 );
 registerServiceWorker();
