7d972449baf5cf0c86ca86bc04ddb75e29c5026d
Yun <indl1670@gmail.com>
2023-01-31 14:43:28 +0900

    chore:BoardItem.jsx 수정

--- frontend/src/components/MyPage/Items/BoardsItem.jsx
+++ frontend/src/components/MyPage/Items/BoardsItem.jsx
@@ -6,14 +6,12 @@ export default function ReviewItem({ item }) {
 
   return (
     <tr className={styles.items}>
-      <td className={styles.type}>
-        {item.typeCode === 0 ? '[입양후기]' : item.typeCode === 1 ? '[무료나눔]' : '[정보공유]'}
-      </td>
-      <td className={styles.title}>{item.title}</td>
-      <td className={styles.author}>{item.author}</td>
-      <td className={styles.count}>{item.count}</td>
+      <td className={styles.type}>{typeCode === 0 ? '[입양후기]' : typeCode === 1 ? '[무료나눔]' : '[정보공유]'}</td>
+      <td className={styles.title}>{title}</td>
+      <td className={styles.author}>{author}</td>
+      <td className={styles.count}>{count}</td>
       <td className={styles.registDate}>
-        {item.registDate[0]}.{item.registDate[1]}.{item.registDate[2]}
+        {registDate[0]}.{registDate[1]}.{registDate[2]}
       </td>
     </tr>
   );
