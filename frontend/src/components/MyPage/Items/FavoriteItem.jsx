681f24bc18cb92868b41b19d9f5b8c0059cb8475
Yun <indl1670@gmail.com>
2023-02-01 03:09:08 +0900

    #S08P12E104-180 #comment feat:feature/front/mypage:[마이페이지(개인) 꾹리스트 구현 (완료)]

--- frontend/src/components/MyPage/Items/FavoriteItem.jsx
+++ frontend/src/components/MyPage/Items/FavoriteItem.jsx
@@ -1,4 +1,25 @@
 import React from 'react';
 import styles from './FavoriteItem.module.scss';
+import Card from 'components/Common/Card';
+import FavImg from 'assets/images/fav-img.png';
 
-export default function FavoriteItem() {}
+export default function FavoriteItem({ item }) {
+  const { image, name, stateCode } = item;
+
+  return (
+    <div className={styles.card}>
+      <Card>
+        <div>
+          <div className={stateCode === 0 ? styles.state0 : stateCode === 1 ? styles.state1 : styles.state2}>
+            {stateCode === 0 ? '공고' : stateCode === 1 ? '보호중' : '입양완료'}
+          </div>
+          <img src={image} alt="" />
+          <img title="꾹 해제" className={styles['fav-img']} src={FavImg} alt="" />
+        </div>
+        <div className={styles.description}>
+          <div className={styles.name}>{name}</div>
+        </div>
+      </Card>
+    </div>
+  );
+}
--- frontend/src/components/MyPage/Lists/FavoriteList.jsx
+++ frontend/src/components/MyPage/Lists/FavoriteList.jsx
@@ -1,4 +1,23 @@
+import { useQuery } from '@tanstack/react-query';
+import { getFavListListOfUser } from 'apis/api/user';
 import React from 'react';
 import styles from './FavoriteList.module.scss';
+import FavoriteItem from '../Items/FavoriteItem';
 
-export default function FavoriteList() {}
+export default function FavoriteList() {
+  const { data, isLoading } = useQuery({
+    queryKey: ['myFavoriteList'],
+    queryFn: () => getFavListListOfUser(),
+  });
+
+  if (isLoading) return;
+
+  console.log(data);
+  return (
+    <div className={styles.container}>
+      {data.map(item => (
+        <FavoriteItem key={item.dogId} item={item} />
+      ))}
+    </div>
+  );
+}
