a4cc0325e0158185df663b078e4253f3bda76a61
Yun <indl1670@gmail.com>
2023-02-01 09:05:48 +0900

    chore:missing handler 수정

--- frontend/src/mocks/handlers/missing.js
+++ frontend/src/mocks/handlers/missing.js
@@ -2,7 +2,7 @@ import { missingDogList } from 'mocks/data/missing';
 import { rest } from 'msw';
 
 export const missing = [
-  rest.get(`${process.env.REACT_APP_API_URL}/reports/missings`, (req, res, ctx) => {
+  rest.get(`${process.env.REACT_APP_API_URL}/reports/missings/:userId`, (req, res, ctx) => {
     const code = req.url.searchParams.get('code');
     if (code === 0) {
       return res(ctx.status(200), ctx.json(missingDogList));
