7ae5da346d309d6dc92cb405100999dcda0f273c
Yun <indl1670@gmail.com>
2023-02-02 14:22:11 +0900

    #S08P12E104-172 #comment feat:feature/front/shelter:[보호소 리스트 컴포넌트 구현 (완료)]

--- frontend/src/components/Shelters/List/SheltersList.jsx
+++ frontend/src/components/Shelters/List/SheltersList.jsx
@@ -1,5 +1,23 @@
+import { useQuery } from '@tanstack/react-query';
+import { getShelterList } from 'apis/api/shelter';
 import React from 'react';
+import styles from './SheltersList.module.scss';
+import SheltersItem from '../Items/SheltersItem';
 
-export default function SheltersList() {
-  return;
+export default function SheltersList({ sido, gugun, breed }) {
+  const { data, isLoading } = useQuery({
+    queryKey: ['searchAllShelters'],
+    queryFn: () => getShelterList(sido, gugun, breed),
+  });
+
+  if (isLoading) return;
+  console.log(data);
+
+  return (
+    <div className={styles.container}>
+      {data.map(item => (
+        <SheltersItem key={item.shelterId} item={item} />
+      ))}
+    </div>
+  );
 }
--- frontend/src/pages/Shelters/Shelters.jsx
+++ frontend/src/pages/Shelters/Shelters.jsx
@@ -1,48 +1,11 @@
-import { useQuery } from '@tanstack/react-query';
-import { getRegionList } from 'apis/utils/parsingRegionData';
-import { getCountShelterByBreed } from 'apis/api/shelter';
-import React, { useState } from 'react';
+import React from 'react';
 import styles from './Shelters.module.scss';
-import SheltersList from 'components/Shelters/List/SheltersList';
+import Contents from 'components/Shelters/Contents';
 
-export default function Shelters() {
-  const { data, isLoading } = useQuery({
-    queryKey: ['searchBreedCountPerRegion'],
-    queryFn: () => getCountShelterByBreed(null),
-  });
-
-  const [click, setClick] = useState(Array(17).fill(false));
-  const [sido, setSido] = useState();
-
-  console.log(data);
-  if (isLoading) return;
+export default function MyPage() {
   return (
     <div className={styles.container}>
-      <div className={styles['map-wrapper']}>
-        <svg className={styles.svg} viewBox="30 100 400 800" xmlns="http://www.w3.org/2000/svg">
-          <g>
-            {getRegionList(data.list).map((item, idx) => {
-              return (
-                <path
-                  key={item.code}
-                  className={click === undefined ? styles.land : click[idx] ? styles['land-click'] : styles.land}
-                  onClick={() => {
-                    const newClick = Array(17).fill(false);
-                    newClick[idx] = true;
-                    setClick(newClick);
-                    setSido(item.code);
-                  }}
-                  title={item.name}
-                  d={item.d}
-                />
-              );
-            })}
-          </g>
-        </svg>
-      </div>
-      <div className={styles.lists}>
-        <SheltersList />
-      </div>
+      <Contents />
     </div>
   );
 }
