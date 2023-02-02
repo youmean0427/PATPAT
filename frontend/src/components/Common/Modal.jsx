bfa2928938bdcacc73fdf14276d69e4de6ce62a0
Yun <indl1670@gmail.com>
2023-02-03 00:59:13 +0900

    chore:Modal 컴포넌트 구현

---
+++ frontend/src/components/Common/Modal.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import styles from './Modal.module.scss';
+
+export default function Modal(props) {
+  const { open, close, title } = props;
+  return (
+    <div className={open ? `${styles.openModal} ${styles.modal}` : styles.modal}>
+      {open ? (
+        <section>
+          <header>
+            {title}
+            <button className={styles.close} onClick={close}>
+              &times;
+            </button>
+          </header>
+          <main className={styles['modal-main']}>{props.children}</main>
+          <footer>
+            <button className={styles.save} onClick="">
+              저장
+            </button>
+            <button className={styles.close} onClick={close}>
+              취소
+            </button>
+          </footer>
+        </section>
+      ) : null}
+    </div>
+  );
+}
