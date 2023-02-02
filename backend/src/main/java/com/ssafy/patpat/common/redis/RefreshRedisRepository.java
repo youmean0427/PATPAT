490f778616a8953bca62917dfb784f14167480cf
rudgns9334 <rudgns9334@gmail.com>
2023-02-02 17:30:03 +0900

    #S08P12E104-227 #in-progress #comment feature/back/login[redis설정]

---
+++ backend/src/main/java/com/ssafy/patpat/common/redis/RefreshRedisRepository.java
@@ -0,0 +1,6 @@
+package com.ssafy.patpat.common.redis;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface RefreshRedisRepository extends CrudRepository<RefreshRedis, String> {
+}
