1de0f1490e52d5f555f081d04f8b0cc1dba6350e
tjrghks96 <tjrghks96@naver.com>
2023-02-03 14:37:42 +0900

    S08P12E104-167 #comment feat:feature/front/header:[scroll 시 Header css 변경]

--- frontend/src/components/Common/Header/Header.jsx
+++ frontend/src/components/Common/Header/Header.jsx
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useCallback, useEffect } from 'react';
 import styles from './Header.module.scss';
 import Container from '../Container';
 import Navbar from './Navbar/Navbar';
 export default function Header() {
+  const handleScroll = useCallback(() => {
+    const header = document.querySelector(`.${styles.header}`);
+    if (window.scrollY === 0) {
+      header.classList.remove(styles.scroll);
+    } else {
+      header.classList.add(styles.scroll);
+    }
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
   return (
     <header className={styles.header}>
       <nav className={styles.nav}>
