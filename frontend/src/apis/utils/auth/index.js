378414d4317213d18f0d15a861aef62d810babe5
tjrghks96 <tjrghks96@naver.com>
2023-02-03 14:03:30 +0900

    #S08P12E104-225 #comment feat:feature/front/logout:[로그아웃 기능 구현, 로그인 로그아웃시 헤더 변경]

--- frontend/src/apis/utils/auth/index.js
+++ frontend/src/apis/utils/auth/index.js
@@ -1,14 +1,16 @@
 import { defaultInstance } from 'apis/utils/index';
+import { useRecoilState } from 'recoil';
+import { isLoginState } from 'recoil/atoms/user';
 
-export const login = async (type, code) => {
+export const login = async (provider, code) => {
   try {
-    const res = await defaultInstance.get(`http://i8e104.p.ssafy.io:8081/user/login/${type}?code=${code}`);
+    const res = await defaultInstance.get(`http://i8e104.p.ssafy.io:8081/user/login/${provider}?code=${code}`);
     const accessToken = res.data.tokenDto.accessToken;
     const refreshToken = res.data.tokenDto.refreshToken;
     const userInfo = res.data.userDto;
 
-    localStorage.setItem('accessToken', accessToken);
-    localStorage.setItem('refreshToken', refreshToken);
+    localStorage.setItem('accessToken', `Bearer ${accessToken}`);
+    localStorage.setItem('refreshToken', `Bearer ${refreshToken}`);
     localStorage.setItem('isLogin', true);
     localStorage.setItem(
       'user',
@@ -23,9 +25,21 @@ export const login = async (type, code) => {
   }
 };
 
-export const logout = () => {
-  localStorage.removeItem('isLogin');
-  localStorage.removeItem('user');
-  localStorage.removeItem('refreshToken');
-  localStorage.removeItem('accessToken');
+export const logout = async () => {
+  const refresh = localStorage.getItem('refreshToken');
+  const access = localStorage.getItem('accessToken');
+  const res = await defaultInstance.get(`${process.env.REACT_APP_API_URL}/user/logout`, {
+    headers: {
+      RefreshToken: refresh,
+      AccessToken: access,
+    },
+  });
+  if (res.data.result === 'success') {
+    localStorage.removeItem('isLogin');
+    localStorage.removeItem('user');
+    localStorage.removeItem('refreshToken');
+    localStorage.removeItem('accessToken');
+  } else {
+    // 로그아웃에 실패한 경우
+  }
 };
