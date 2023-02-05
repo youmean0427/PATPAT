532e1fce74b2315456ce7fd87308195ca7f3465c
kamoo2 <kamoon2da@gmail.com>
2023-02-05 13:11:15 +0900

    chore : console.log 제거 , 백 연동으로 인한 변수명 변경

--- frontend/src/pages/Mbti/MoreInfo.jsx
+++ frontend/src/pages/Mbti/MoreInfo.jsx
@@ -8,25 +8,24 @@ import styles from './MoreInfo.module.scss';
 
 export default function MoreInfo() {
   const {
-    state: { breedId, breedName, breedImg },
+    state: { breedId, breedName, thumbnail },
   } = useLocation();
   const { data, isLoading } = useQuery(['searchBreedCountPerRegion'], () => getCountShelterByBreed(breedId));
   if (isLoading) return;
-
   return (
     <MbtiContainer>
       <div className={styles.card}>
         <div className={styles['img-box']}>
-          <img src={breedImg} alt="견종 이미지" />
+          <img src={thumbnail} alt="견종 이미지" />
         </div>
         <div className={styles['desc-box']}>
           <div className={styles['desc-breed']}>{breedName}</div>
           <div className={styles['desc-count']}>
-            PATPAT에 등록된 보호소 중 해당 견종을 보유한 곳은 총 <span>{data.totalCnt}개</span>입니다
+            PATPAT에 등록된 보호소 중 해당 견종을 보유한 곳은 총 <span>80개</span>입니다
           </div>
         </div>
       </div>
-      <SouthKorea breedId={breedId} breedName={breedName} list={data.list} />
+      <SouthKorea breedId={breedId} breedName={breedName} list={data} />
     </MbtiContainer>
   );
 }
