b63749703638abf206c00f6ed90ec519c2d1e552
rudgns9334 <rudgns8285@naver.com>
2023-02-05 12:10:25 +0900

    #noid #error #comment [로그인/로그아웃에러수정]

--- backend/src/main/java/com/ssafy/patpat/common/entity/Image.java
+++ backend/src/main/java/com/ssafy/patpat/common/entity/Image.java
@@ -1,25 +1,22 @@
 package com.ssafy.patpat.common.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import java.math.BigInteger;
 
 @Entity
 @Builder
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Image {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int imageId;
+    private Integer imageId;
 
     private String origFilename;
     private String filename;
