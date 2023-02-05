86d0eeb181fd256eed209ad51e6202689b90ecd5
kamoo2 <kamoon2da@gmail.com>
2023-02-05 13:08:37 +0900

    #S08P12E104-169 #comment feat:feature/front/shelter:[보호소 찾기 리스트 , 아이템 컴포넌트 구현완료 , 추가적인 util 함수 구현 , recoil 시도 구군 견종 선택 상태 전역으로 관리]

--- frontend/src/components/SearchShelter/ShelterItem.jsx
+++ frontend/src/components/SearchShelter/ShelterItem.jsx
@@ -1,5 +1,20 @@
 import React from 'react';
-
-export default function ShelterItem() {
-  return <div>ShelterItem</div>;
+import styles from './ShelterItem.module.scss';
+import shelter from 'assets/images/default-shelter.png';
+import { useNavigate } from 'react-router-dom';
+export default function ShelterItem({ item }) {
+  const navigate = useNavigate();
+  return (
+    <div
+      onClick={() => {
+        navigate(`/shelter/${item.shelterId}`);
+      }}
+      className={styles.container}
+    >
+      <div className={styles.thumbnail}>
+        <img src={shelter} alt="shelter" />
+      </div>
+      <div className={styles.info}>{item.name}</div>
+    </div>
+  );
 }
--- frontend/src/components/SearchShelter/ShelterList.jsx
+++ frontend/src/components/SearchShelter/ShelterList.jsx
@@ -1,5 +1,40 @@
+import { useQuery } from '@tanstack/react-query';
+import { getShelterList } from 'apis/api/shelter';
 import React from 'react';
+import { useRecoilValue } from 'recoil';
+import { selectBreedState, selectGugunState, selectSidoState } from 'recoil/atoms/shelter';
+import ShelterItem from './ShelterItem';
+import styles from './ShelterList.module.scss';
 
-export default function ShelterList() {
-  return <div>ShelterList</div>;
+const list = [
+  { shelterId: 1, name: '초량 보호소' },
+  { shelterId: 2, name: '초량 보호소' },
+  { shelterId: 3, name: '초량 보호소' },
+  { shelterId: 4, name: '초량 보호소' },
+  { shelterId: 5, name: '초량 보호소' },
+  { shelterId: 6, name: '초량 보호소' },
+  { shelterId: 7, name: '초량 보호소' },
+  { shelterId: 8, name: '초량 보호소' },
+  { shelterId: 9, name: '초량 보호소' },
+  { shelterId: 10, name: '초량 보호소' },
+  { shelterId: 11, name: '초량 보호소' },
+  { shelterId: 12, name: '초량 보호소' },
+];
+function ShelterList() {
+  const { sidoCode } = useRecoilValue(selectSidoState);
+  const { gugunCode } = useRecoilValue(selectGugunState);
+  const { breedId } = useRecoilValue(selectBreedState);
+  const { data, isLoading } = useQuery(['shelterList', sidoCode, gugunCode, breedId], () => {
+    return getShelterList(sidoCode, gugunCode, breedId, 4, 0);
+  });
+  if (isLoading) return;
+  return (
+    <div className={styles.container}>
+      {list.map((item, index) => {
+        return <ShelterItem key={index} item={item} />;
+      })}
+    </div>
+  );
 }
+
+export default React.memo(ShelterList);
