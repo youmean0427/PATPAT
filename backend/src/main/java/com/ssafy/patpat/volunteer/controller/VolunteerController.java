e1f543d0b5861765062f7a2f063336601810bafa
rudgns9334 <rudgns8285@naver.com>
2023-02-07 05:46:43 +0900

    #S08P12E104-213 #done #comment feature/back/volunteer[봉사 등록 구현]

--- backend/src/main/java/com/ssafy/patpat/volunteer/controller/VolunteerController.java
+++ backend/src/main/java/com/ssafy/patpat/volunteer/controller/VolunteerController.java
@@ -2,9 +2,7 @@ package com.ssafy.patpat.volunteer.controller;
 
 import com.ssafy.patpat.common.dto.ResponseMessage;
 import com.ssafy.patpat.volunteer.dto.*;
-import com.ssafy.patpat.volunteer.entity.VolunteerSchedule;
-import com.ssafy.patpat.volunteer.service.VolunteerNoticeService;
-import com.ssafy.patpat.volunteer.service.VolunteerScheduleService;
+import com.ssafy.patpat.volunteer.service.VolunteerService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
@@ -21,8 +19,7 @@ import java.util.List;
 @Api(tags = {"07. Volunteer"},description = "봉사 관련 서비스")
 public class VolunteerController {
 
-    private final VolunteerScheduleService volunteerScheduleService;
-    private final VolunteerNoticeService volunteerNoticeService;
+    private final VolunteerService volunteerService;
 
     /**
      * 봉사 공고 조회(전체)
@@ -34,7 +31,7 @@ public class VolunteerController {
     @ApiOperation(value = "봉사 공고 조회", notes = "구군 봉사 공고를 조회(gugunCode)/개인의 보호소 페이지 공고 조회(shelterId)")
     public ResponseEntity<Object> selectNoticeList(RequestVolunteerDto requestVolunteerDto){
         //서비스 호출 코드
-        List<VolunteerNoticeDto> list = volunteerNoticeService.selectNoticeList(requestVolunteerDto);
+        List<VolunteerNoticeDto> list = volunteerService.selectNoticeList(requestVolunteerDto);
         if(list != null){
             return ResponseEntity.status(HttpStatus.OK)
                     .body(list);
@@ -53,7 +50,7 @@ public class VolunteerController {
     @ApiOperation(value = "일별 봉사 공고 조회", notes = "일별 상세 조회 - 파라미터로 noticeId")
     public ResponseEntity<Object> selectScheduleList(@RequestParam("noticeId") Long noticeId){
         //서비스 호출 코드
-        List<VolunteerScheduleDto> list = volunteerScheduleService.selectScheduleList(noticeId);
+        List<VolunteerScheduleDto> list = volunteerService.selectScheduleList(noticeId);
         if(list != null){
             return ResponseEntity.status(HttpStatus.OK)
                     .body(list);
@@ -71,7 +68,7 @@ public class VolunteerController {
     @ApiOperation(value = "봉사 공고 조회", notes = "보호소가 자기 봉사 공고 볼 경우 파라미터로 year, month, shelterId")
     public ResponseEntity<Object> selectNoticeListByMonth(VolunteerMonthDto volunteerMonthDto){
         //서비스 호출 코드
-        List<VolunteerNoticeDto> list = volunteerNoticeService.selectNoticeListByMonth(volunteerMonthDto);
+        List<VolunteerNoticeDto> list = volunteerService.selectNoticeListByMonth(volunteerMonthDto);
         if(true){
             return ResponseEntity.status(HttpStatus.OK)
                     .body(new ArrayList<NoticeDto>());
@@ -80,22 +77,7 @@ public class VolunteerController {
                     .body(new ResponseMessage("FAIL"));
         }
     }
-    /**
-     * 봉사 공고 상세 조회
-     * @return
-     */
-    @GetMapping("/notices/detail/{noticeId}")
-    @ApiOperation(value = "봉사 공고 상세 조회", notes = "봉사 공고 상세 조회")
-    public ResponseEntity<Object> detailNotice(@PathVariable int noticeId){
-        //서비스 호출 코드
-        if(true){
-            return ResponseEntity.status(HttpStatus.OK)
-                    .body(new NoticeDto());
-        }else{
-            return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                    .body(new ResponseMessage("FAIL"));
-        }
-    }
+
     /**
      * 봉사 공고 등록
      * @return
@@ -104,7 +86,7 @@ public class VolunteerController {
     @ApiOperation(value = "봉사 공고 등록", notes = "봉사 공고 등록")
     public ResponseEntity<Object> insertNotice(@RequestBody NoticeDto noticeDto){
         //서비스 호출 코드
-        if(true){
+        if(volunteerService.insertNotice(noticeDto)){
             return ResponseEntity.status(HttpStatus.OK)
                     .body(new ResponseMessage("SUCCESS"));
         }else{
---
+++ backend/src/main/java/com/ssafy/patpat/volunteer/service/VolunteerService.java
@@ -0,0 +1,184 @@
+package com.ssafy.patpat.volunteer.service;
+
+import com.ssafy.patpat.common.code.Reservation;
+import com.ssafy.patpat.shelter.entity.Shelter;
+import com.ssafy.patpat.shelter.repository.ShelterRepository;
+import com.ssafy.patpat.user.service.UserService;
+import com.ssafy.patpat.volunteer.dto.*;
+import com.ssafy.patpat.volunteer.entity.VolunteerNotice;
+import com.ssafy.patpat.volunteer.entity.VolunteerReservation;
+import com.ssafy.patpat.volunteer.entity.VolunteerSchedule;
+import com.ssafy.patpat.volunteer.repository.VolunteerNoticeRepository;
+import com.ssafy.patpat.volunteer.repository.VolunteerReservationRepository;
+import com.ssafy.patpat.volunteer.repository.VolunteerScheduleRepository;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class VolunteerService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(VolunteerService.class);
+
+    private final VolunteerNoticeRepository volunteerNoticeRepository;
+    private final VolunteerScheduleRepository volunteerScheduleRepository;
+    private final VolunteerReservationRepository volunteerReservationRepository;
+
+    private final ShelterRepository shelterRepository;
+
+    /**
+     * 봉사 공고 조회(전체)
+     * 봉사 찾기로 구군으로 탐색 했을 경우
+     * 개인이 보호소 페이지에서 보는 경우
+     * @return
+     */
+    @Transactional
+    public List<VolunteerNoticeDto> selectNoticeList(RequestVolunteerDto requestVolunteerDto){
+        PageRequest pageRequest = PageRequest.of(requestVolunteerDto.getOffset(),requestVolunteerDto.getLimit(), Sort.by("volunteerDate").ascending());
+        List<VolunteerNotice> volunteerNotices;
+        if(requestVolunteerDto.getGugunCode() != null){
+            volunteerNotices = volunteerNoticeRepository.findWithShelterByShelterGugunCodeAndReservationStateCodeAndVolunteerDateGreaterThan(requestVolunteerDto.getGugunCode(), Reservation.대기중, LocalDate.now().toString(), pageRequest);
+        }else if(requestVolunteerDto.getShelterId() != null){
+            volunteerNotices = volunteerNoticeRepository.findWithShelterByShelterShelterIdAndReservationStateCodeAndVolunteerDateGreaterThan(requestVolunteerDto.getShelterId(), Reservation.대기중, LocalDate.now().toString(), pageRequest);
+        }else{
+            LOGGER.info("탐색 기준이 없습니다.");
+            return null;
+        }
+        if(volunteerNotices.isEmpty()){
+            LOGGER.info("검색되는 봉사 공고가 없습니다.");
+            return null;
+        }
+        List<VolunteerNoticeDto> list = new ArrayList<>();
+        for (VolunteerNotice vn:
+             volunteerNotices) {
+            list.add(VolunteerNoticeDto.builder()
+                    .name(vn.getShelter().getName())
+                    .noticeId(vn.getNoticeId())
+                    .shelterId(vn.getShelter().getShelterId())
+                    .state(vn.getReservationStateCode().name())
+                    .stateCode(vn.getReservationStateCode().getCode())
+                    .title(vn.getTitle())
+                    .volunteerDate(vn.getVolunteerDate())
+                    .build());
+        }
+        return list;
+    }
+
+    @Transactional
+    public List<VolunteerNoticeDto> selectNoticeListByMonth(VolunteerMonthDto volunteerMonthDto){
+        List<VolunteerNotice> volunteerNotices = volunteerNoticeRepository.findWithShelterByShelterShelterIdAndVolunteerDateLikeOrderByVolunteerDateAsc(volunteerMonthDto.getShelterId(), volunteerMonthDto.getYear()+"-"+volunteerMonthDto.getMonth());
+        if(volunteerNotices.isEmpty()){
+            LOGGER.info("봉사 공고가 비었습니다.");
+            return null;
+        }
+        List<VolunteerNoticeDto> list = new ArrayList<>();
+        for (VolunteerNotice vn:
+                volunteerNotices) {
+            list.add(VolunteerNoticeDto.builder()
+                    .name(vn.getShelter().getName())
+                    .noticeId(vn.getNoticeId())
+                    .shelterId(vn.getShelter().getShelterId())
+                    .state(vn.getReservationStateCode().name())
+                    .stateCode(vn.getReservationStateCode().getCode())
+                    .title(vn.getTitle())
+                    .volunteerDate(vn.getVolunteerDate())
+                    .build());
+        }
+        return list;
+    }
+
+
+    /** 개인이 구군 정보를 이용
+     * 특정 봉사일정을 보고 싶을 때
+     * */
+    @Transactional
+    public List<VolunteerScheduleDto> selectScheduleList(Long noticeId){
+
+        List<VolunteerSchedule> volunteerSchedules = volunteerScheduleRepository.findWithVolunteerNoticeByVolunteerNoticeNoticeIdAndReservationStateCode(noticeId, Reservation.대기중);
+        if(volunteerSchedules.isEmpty()){
+            LOGGER.info("봉사 일정이 비었습니다.");
+            return null;
+        }
+        List<VolunteerScheduleDto> list = new ArrayList<>();
+        for (VolunteerSchedule vs:
+                volunteerSchedules) {
+            int capacity = 0;
+            List<VolunteerReservation> volunteerReservations = volunteerReservationRepository.findWithVolunteerScheduleByVolunteerScheduleScheduleIdAndReservationStateCode(vs.getScheduleId(), Reservation.수락);
+            if(!volunteerReservations.isEmpty()){
+                for (VolunteerReservation vr:
+                        volunteerReservations) {
+                    capacity += vr.getCapacity();
+                }
+            }
+
+            list.add(VolunteerScheduleDto.builder()
+                    .scheduleId(vs.getScheduleId())
+                    .startTime(vs.getStartTime())
+                    .endTime(vs.getEndTime())
+                    .totalCapacity(vs.getCapacity())
+                    .capacity(capacity)
+                    .guideLine(vs.getGuideLine())
+                    .reservationState(vs.getReservationStateCode().name())
+                    .reservationStateCode(vs.getReservationStateCode().getCode())
+                    .build());
+        }
+        return list;
+    }
+
+    @Transactional
+    public boolean insertNotice(NoticeDto noticeDto){
+
+        if(noticeDto.getShelterId() == null){
+            LOGGER.info("보호소 Id가 없습니다.");
+            return false;
+        }
+        Optional<Shelter> s = shelterRepository.findById(noticeDto.getShelterId());
+
+        if(!s.isPresent()){
+            LOGGER.info("보호소 조회에 실패했습니다.");
+            return false;
+        }
+
+        // notice 정보
+        VolunteerNotice volunteerNotice = VolunteerNotice.builder()
+                .title(noticeDto.getTitle())
+                .volunteerDate(LocalDate.now().toString())
+                .shelter(s.get())
+                .build();
+        VolunteerNotice v = volunteerNoticeRepository.save(volunteerNotice);
+
+        // schedule 정보
+        List<VolunteerSchedule> volunteerSchedules = new ArrayList<>();
+        if(noticeDto.getList().isEmpty()){
+            // 아무 등록이 없을 때
+            LOGGER.info("등록된 봉사 일정이 없습니다.");
+            return false;
+        }
+        for (VolunteerScheduleDto vs:
+                noticeDto.getList()) {
+            volunteerSchedules.add(VolunteerSchedule.builder()
+                    .capacity(vs.getTotalCapacity())
+                    .startTime(vs.getStartTime())
+                    .endTime(vs.getEndTime())
+                    .guideLine(vs.getGuideLine())
+                    .volunteerNotice(v)
+                    .build());
+        }
+        volunteerScheduleRepository.saveAll(volunteerSchedules);
+
+        return true;
+    }
+
+
+
+}
