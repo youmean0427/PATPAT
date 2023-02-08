d307a22b69d4cdde3ef283e4ec8d03b7141bb5a8
kamoo2 <kamoon2da@gmail.com>
2023-02-09 06:40:56 +0900

    chore : List API Response Data 변경으로 인한 수정

--- frontend/src/components/MyPage/Lists/ShareList.jsx
+++ frontend/src/components/MyPage/Lists/ShareList.jsx
@@ -7,10 +7,11 @@ import Table from 'components/Common/Table';
 export default function ShareList({ code }) {
   const { data, isLoading } = useQuery({
     queryKey: ['myBoardList'],
-    queryFn: () => getBoardListByMe(20, 0, code),
+    queryFn: () => getBoardListByMe(code, 20, 0),
   });
 
   if (isLoading) return;
+  console.log(data);
   return (
     <Table>
       {data.map(item => (
--- frontend/src/components/Report/MissingDog/MissingDogList.jsx
+++ frontend/src/components/Report/MissingDog/MissingDogList.jsx
@@ -56,7 +56,7 @@ export default function MissingDogList() {
       </div>
       <div className={styles.container}>
         <div className={styles.list}>
-          {data.map((item, index) => (
+          {data.list.map((item, index) => (
             <MissingDogItem key={index} item={item} />
           ))}
         </div>
