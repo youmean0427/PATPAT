9dca7c5fc3683a5eabb09229d8a05208e38ee58b
changgohap <changgohap@gmail.com>
2023-02-10 17:42:04 +0900

    chore : 안쓰는 컴포넌트 삭제

--- frontend/src/components/Community/table/CommunityTableRow.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from './CommunityTable.module.scss';
-
-const CommunityTableRow = ({ children }) => {
-  return <tr className={styles['community-table-row']}>{children}</tr>;
-};
-
-export default CommunityTableRow;
