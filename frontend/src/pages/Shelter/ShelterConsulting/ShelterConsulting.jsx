f972670037a6905e6f6803eae4d7a47d10553449
kamoo2 <kamoon2da@gmail.com>
2023-02-12 01:01:08 +0900

    #S08P12E104-262 #comment feat:feature/front/shelter/consulting:[ShelterConsulting 필요없는 부분 제거]

--- frontend/src/pages/Shelter/ShelterConsulting/ShelterConsulting.jsx
+++ frontend/src/pages/Shelter/ShelterConsulting/ShelterConsulting.jsx
@@ -1,24 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ConsultingList from 'components/ShelterPage/Lists/ConsultingList';
-import Modal from 'components/Common/Modal';
 import styles from './ShelterConsulting.module.scss';
-import { useLocation } from 'react-router-dom';
 import ShelterContainer from 'containers/ShelterContainer';
+import { useRecoilValue } from 'recoil';
+import { myShelterIdState } from 'recoil/atoms/user';
 export default function ShelterConsulting() {
-  const [click, setClick] = useState([false, false, false, true]);
-  const [modal, setModal] = useState(false);
-  const location = useLocation();
-  const openModal = () => {
-    setModal(true);
-  };
-  const closeModal = () => {
-    setModal(false);
-  };
-
+  const myShelterId = useRecoilValue(myShelterIdState);
   return (
     <ShelterContainer title="상담 관리">
       <div className={styles.list}>
-        <ConsultingList shelterId={location.state.shelterId} />
+        <ConsultingList shelterId={myShelterId} />
       </div>
     </ShelterContainer>
   );
