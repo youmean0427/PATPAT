71c89e876e58045c4a2f4e7d397f2f3fa28040b8
kamoo2 <kamoon2da@gmail.com>
2023-02-12 00:39:16 +0900

    chore : progress 추가, api 수정

--- frontend/src/apis/api/protect.js
+++ frontend/src/apis/api/protect.js
@@ -5,12 +5,15 @@ import { authInstance, defaultInstance } from 'apis/utils';
 /**
  * GET : 보호소 보호동물 리스트
  * @param {int} code 0 : 안락사 , 1 : 최신순
+ * @param {int} stateCode 0 : 공고중 , 1 : 보호중 , 2 : 입양예정 , 3 : 입양 , 4 : 자연사 , 5 : 안락사
  * @param {int} limit 리스트로 가져올 item 개수
  * @param {int} offset skip 할 개수
  * @returns [] : code에 따른 Item을 가진 배열
  */
-export const getProtectList = async (code, limit, offset) => {
-  const { data } = await defaultInstance.get(`/protects?code=${code}&limit=${limit}&offSet=${offset}`);
+export const getProtectList = async (code, stateCode, limit, offset) => {
+  const { data } = await defaultInstance.get(
+    `/protects?code=${code}&stateCode=${stateCode}&limit=${limit}&offSet=${offset}`
+  );
   return data;
 };
 
