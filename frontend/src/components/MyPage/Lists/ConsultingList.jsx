2e7a8eb4d2bd9c04adf17f526d5e6f9c2258dbc4
Yun <indl1670@gmail.com>
2023-02-13 03:12:21 +0900

    #S08P12E104-250 #comment feat:feature/front/mypage:[마이페이지(개인) 상담신청 UI 수정 (진행중)]

--- frontend/src/components/MyPage/Lists/ConsultingList.jsx
+++ frontend/src/components/MyPage/Lists/ConsultingList.jsx
@@ -1,18 +1,14 @@
 import { useQuery } from '@tanstack/react-query';
 import { getMyConsultations } from 'apis/api/consulting';
 import React, { useState } from 'react';
-import styles from './ReservationList.module.scss';
 import ConsultingItem from '../Items/ConsultingItem';
 import { MdArrowForwardIos, MdArrowBackIosNew } from 'react-icons/md';
+import styles from './ConsultingList.module.scss';
+import NoData from 'components/Common/NoData';
 
-export default function ConsultingList({ userId }) {
+export default function ConsultingList({ stateCode }) {
   const [page, setPage] = useState(1);
   const LIMIT = 8;
-
-  const { data, isLoading } = useQuery({
-    queryKey: ['myConsultingList'],
-    queryFn: () => getMyConsultations(LIMIT, page - 1, 7),
-  });
 
   const handleClickPrev = () => {
     setPage(prev => prev - 1);
@@ -22,36 +18,46 @@ export default function ConsultingList({ userId }) {
     setPage(prev => prev + 1);
   };
 
-  if (isLoading) return;
+  const { data, isLoading } = useQuery({
+    queryKey: ['getMyConsultations', stateCode],
+    queryFn: () => {
+      return getMyConsultations(LIMIT, page - 1, stateCode);
+    },
+  });
 
+  if (isLoading) return;
   return (
     <div>
-      <div className={styles.pagination}>
-        <button
-          onClick={handleClickPrev}
-          className={page === 1 ? `${styles.button} ${styles.disabled}` : styles.button}
-          disabled={page === 1 ? true : false}
-        >
-          <MdArrowBackIosNew />
-        </button>
-        <span>{page}</span>
-        <button
-          onClick={handleClickNext}
-          className={
-            data.totalPage === 0 || page === data.totalPage ? `${styles.button} ${styles.disabled}` : styles.button
-          }
-          disabled={data.totalPage === 0 || page === data.totalPage ? true : false}
-        >
-          <MdArrowForwardIos />
-        </button>
-      </div>
-      <div className={styles.container}>
-        {data.totalCount === 0 ? (
-          <div className={styles['no-data']}>신청한 상담 정보가 없습니다.</div>
-        ) : (
-          data.list.map(item => <ConsultingItem key={item.consultingId} item={item} />)
-        )}
-      </div>
+      {data.totalCount === 0 ? (
+        <NoData>신청된 상담 내역이 없습니다.</NoData>
+      ) : (
+        <>
+          <div className={styles.pagination}>
+            <button
+              onClick={handleClickPrev}
+              className={page === 1 ? `${styles.button} ${styles.disabled}` : styles.button}
+              disabled={page === 1 ? true : false}
+            >
+              <MdArrowBackIosNew />
+            </button>
+            <span>{page}</span>
+            <button
+              onClick={handleClickNext}
+              className={
+                data.totalPage === 0 || page === data.totalPage ? `${styles.button} ${styles.disabled}` : styles.button
+              }
+              disabled={data.totalPage === 0 || page === data.totalPage ? true : false}
+            >
+              <MdArrowForwardIos />
+            </button>
+          </div>
+          <div className={styles.list}>
+            {data.list.map(item => (
+              <ConsultingItem key={item.consultingId} item={item} filterCode={stateCode} />
+            ))}
+          </div>
+        </>
+      )}
     </div>
   );
 }
