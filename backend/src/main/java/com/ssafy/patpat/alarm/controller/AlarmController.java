1c4d3bf124dd8b46873d60c3b2e9fbe3b5d0a640
rudgns9334 <rudgns9334@gmail.com>
2023-02-13 15:25:20 +0900

    #noid #done #comment [매핑]

--- backend/src/main/java/com/ssafy/patpat/alarm/controller/AlarmController.java
+++ backend/src/main/java/com/ssafy/patpat/alarm/controller/AlarmController.java
@@ -12,10 +12,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.io.IOException;
@@ -24,7 +21,8 @@ import java.util.concurrent.ConcurrentHashMap;
 
 @RequiredArgsConstructor
 @Slf4j
-@RestController("/api/alarm")
+@RestController
+@RequestMapping("/api/alarm")
 public class AlarmController {
     private static final Logger LOGGER = LoggerFactory.getLogger(AlarmController.class);
     public static Map<Long, SseEmitter> sseEmitters = new ConcurrentHashMap<>();
